e5143c5ea15e482aea4a7471f7caee1cb292fdd5
levislell <levislelait719@icloud.com>
2026-07-14 19:09:28 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -98,7 +98,8 @@ EMOTES = {
     "sumo": "emote-sumo", "death2": "emote-death2", "smirking": "emoji-smirking",
     "voguehands": "dance-voguehands", "eyeroll": "emoji-eyeroll", "giveup": "emoji-give-up",
     "bunnyhop": "emote-bunnyhop", "exasperatedb": "emote-exasperatedb", "loophappy": "idle-loop-happy",
-    "heartfingers": "emote-heartfingers", "collabphoto": "emote-collab-photo-left"
+    "heartfingers": "emote-heartfingers", "collabphoto": "emote-collab-photo-left",
+    "afk": "emote-afk-idle", "afk", "emote-afk-idle"
 }
 
 # ==========================
